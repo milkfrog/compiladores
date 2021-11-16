f8996f20d8635b6f754e2f1f6b054b29250f8c51
continMarcelo <marcelo.contin@bridge.ufsc.br>
2021-11-16 19:34:50 -0300

    Ajustando as pastas pro maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 import java.io.IOException;
 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+
 public class Main {
     public static void main(String[] args) throws IOException {
 
         String path = args[0];
 
-        CharStream charStream = CharStream.fromFileName(path);
+        CharStream charStream = CharStreams.fromFileName(path);
 
     }
 }
