5536a51f0f81f1d7cbc4d7bc29c44ab6a66d6db4
continMarcelo <marcelo.contin@bridge.ufsc.br>
2021-11-16 20:00:34 -0300

    arrumando pom e umas tranqueiras

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +11,8 @@ public class Main {
         String path = args[0];
 
         CharStream charStream = CharStreams.fromFileName(path);
+
+        CC20212Lexer teste = new CC20212Lexer(charStream);
 
     }
 }
